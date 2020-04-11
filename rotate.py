40831e61df6af7b720b04b47d8f35dfa850bc899
William Jones <wtravisjones@gmail.com>
2020-04-11 14:29:25 -0500

    Initial commit

---
+++ rotate.py
@@ -0,0 +1,107 @@
+import math
+import sys
+import os
+import numpy as np
+import pygame
+from pathlib import Path
+
+map_image = os.path.join(Path.home(), "Dropbox/finalmap_0.png")
+size = width, height = 160, 144
+print(size)
+center = x, y = size[0] // 2, size[1] // 2
+black = 0, 0, 0
+screen = None
+zeors_array = np.zeros((2, 3))
+print(zeors_array)
+map = None
+angle = 0
+viewer = x, y = center[0], center[1]
+world = None
+scroll_speed = 2
+
+pygame.init()
+clock = pygame.time.Clock()
+
+
+def render_map():
+    global screen, map, size, viewer, world
+
+    # get top-left starting coord of world map
+    start = x, y = viewer[0] - center[0], viewer[1] - center[1]
+    for scanline_y in range(size[1]):
+        for scanline_x in range(size[0]):
+
+            peek = x, y = start[0] + scanline_x, start[1] + scanline_y
+            if (
+                peek[0] >= 0
+                and peek[0] < world[0]
+                and peek[1] >= 0
+                and peek[1] < world[1]
+            ):
+                color = map.get_at(peek)
+            else:
+                color = (0, 0, 0)
+            screen.set_at((scanline_x, scanline_y), color)
+
+
+def render_viz():
+    global angle
+    p = np.array([[20], [10]])
+
+    rotated = get_rotation(angle).dot(p)
+    plot = x, y = int(rotated[0][0] + center[0]), int(rotated[1][0] + center[1])
+    print(plot)
+    screen.set_at((plot), (20, 100, 50))
+    pygame.draw.circle(screen, 3000, plot, 5)
+
+    angle += 1
+    if angle > 359:
+        angle = 0
+
+
+def main_loop():
+
+    global screen, map, viewer, world
+    screen = pygame.display.set_mode(size, pygame.SCALED)
+
+    map = pygame.image.load(map_image).convert()
+    world = x, y = map.get_width(), map.get_height()
+    viewer = x, y = world[0] // 2, world[1] // 2
+
+    while 1:
+        screen.fill(black)
+
+        render_map()
+        pygame.display.flip()
+        clock.tick(60)
+        handle_input()
+
+
+def handle_input():
+    global viewer, scroll_speed
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN or event.key == pygame.K_ESCAPE:
+                sys.exit()
+    key = pygame.key.get_pressed()
+
+    if key[pygame.K_LEFT]:
+        viewer = viewer[0] - scroll_speed, viewer[1]
+    if key[pygame.K_RIGHT]:
+        viewer = viewer[0] + scroll_speed, viewer[1]
+    if key[pygame.K_UP]:
+        viewer = viewer[0], viewer[1] - scroll_speed
+    if key[pygame.K_DOWN]:
+        viewer = viewer[0], viewer[1] + scroll_speed
+
+
+def get_rotation(angle):
+    c = math.cos(angle * math.pi / 180)
+    s = math.sin(angle * math.pi / 180)
+    return np.array([[c, -s], [s, c]])
+
+
+if __name__ == "__main__":
+    main_loop()
