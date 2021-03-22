3be744a954a94401ad2c58ac10e9c2f33cd9e062
William Jones <wtravisjones@gmail.com>
2021-03-21 20:32:05 -0500

    Seed random with timer by default

--- cursed/main.c
+++ cursed/main.c
@@ -4,12 +4,22 @@
 #include "input.h"
 #include <locale.h>
 #include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 
 #define DELAY 200000
 
 int main(int argc, char* argv[])
 {
+    if (argc == 2) {
+        char* end;
+        const long seed = strtol(argv[1], &end, 10);
+        srand(seed);
+    } else {
+        time_t t;
+        srand((unsigned)time(&t));
+    }
     draw_init();
 
     game_init(g_max_x / 2, g_max_y);
