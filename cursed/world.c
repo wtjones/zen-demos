c105326fa829096aa64572f9fdb0eb074d6bac53
William Jones <wtravisjones@gmail.com>
2021-03-29 21:35:03 -0500

    --amend

--- cursed/world.c
+++ cursed/world.c
@@ -12,7 +12,6 @@ void world_init(int max_x, int max_y)
     world_max_x = max_x <= WORLD_MAX_X ? max_x : WORLD_MAX_X;
     world_max_y = max_y <= WORLD_MAX_Y ? max_y : WORLD_MAX_Y;
 
-    //world_nodes = malloc(sizeof(WorldNode*) * max_x * max_y);
     for (size_t y = 0; y < world_max_y; y++) {
         for (size_t x = 0; x < world_max_x; x++) {
             WorldNode* node = &world_nodes[x][y];
@@ -23,7 +22,6 @@ void world_init(int max_x, int max_y)
 
 WorldNode* get_world_node(int x, int y)
 {
-    //return &world_nodes[y * world_max_x + x];
     return &world_nodes[x][y];
 }
 void move_to(int x, int y, WorldEntity* entity)
@@ -32,5 +30,4 @@ void move_to(int x, int y, WorldEntity* entity)
     entity->x = x;
     entity->y = y;
     world_nodes[x][y].world_entity = entity;
-    //world_nodes[y * world_max_x + x].world_entity = NULL; //entity;
 }
