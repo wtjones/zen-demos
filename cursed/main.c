8a605a671a0efac107d101e381fba1ae79cced8b
William Jones <wtravisjones@gmail.com>
2021-03-30 21:26:57 -0500

    Cleanup ncurses on crash or ctrl-c

--- cursed/main.c
+++ cursed/main.c
@@ -4,14 +4,32 @@
 #include "input.h"
 #include <locale.h>
 #include <ncurses.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
 #define DELAY 200000
 
+static volatile sig_atomic_t signal_exit = false;
+
+void sigsegv_handler(int _)
+{
+    draw_cleanup();
+}
+
+void sigint_handler(int _)
+{
+    printf("sigint");
+    draw_cleanup();
+    signal_exit = true;
+}
+
 int main(int argc, char* argv[])
 {
+    sigset(SIGSEGV, sigsegv_handler);
+    sigset(SIGINT, sigint_handler);
+
     if (argc == 2) {
         char* end;
         const long seed = strtol(argv[1], &end, 10);
@@ -24,9 +42,8 @@ int main(int argc, char* argv[])
     draw_init();
 
     game_init(g_max_x / 2, g_max_y);
-
     bool should_exit = false;
-    while (!should_exit) {
+    while (!should_exit && !signal_exit) {
         draw();
         refresh();
 
