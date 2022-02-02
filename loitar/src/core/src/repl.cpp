a582f9e8563bd5883bfd98e3d33cc705446936d7
William Jones <wtravisjones@gmail.com>
2022-02-01 19:02:02 -0600

    Add parser output trace

--- loitar/src/core/src/repl.cpp
+++ loitar/src/core/src/repl.cpp
@@ -16,6 +16,11 @@ EvaluatorResult Repl::execute(std::string input)
     spdlog::info("repl: parsing {}", input);
     auto parsed_exp = parse(input);
 
+    spdlog::trace("repl: parsed:");
+    for (auto e : parsed_exp) {
+        spdlog::trace("{}: {}", e->name(), e->to_string());
+    }
+
     spdlog::info("repl: evaluating {}", input);
     return evaluate(m_env, parsed_exp);
 }
