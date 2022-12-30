0d8684f9e142e7e560d79bdf6600d620a378b27a
William T Jones <wtravisjones@gmail.com>
2022-12-30 15:20:38 -0600

    Use DJGPP env

--- samples/dos_gcc/run.sh
+++ samples/dos_gcc/run.sh
@@ -1,11 +1,20 @@
-!#/bin/bash
+#!/bin/bash
 
-/opt/djgpp/bin/i586-pc-msdosdjgpp-gcc main.c -o build/a.exe
+set -euo pipefail
 
-if [ $? -ne 0 ]; then
-    echo "run.sh - build failed"
-    exit 1
-fi
+main() {
 
-echo "run.sh - build succeeded"
-dosbox -conf dosbox.conf
+    source $DJGPP_PREFIX/setenv
+
+    gcc main.c -o build/a.exe
+
+    if [ $? -ne 0 ]; then
+        echo "run.sh - build failed"
+        exit 1
+    fi
+
+    echo "run.sh - build succeeded"
+    dosbox -conf dosbox.conf
+}
+
+main
