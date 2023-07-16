56c077b5c337c3c40ac56d17ed9a75d7992f9bb1
William Jones <wtravisjones@gmail.com>
2023-07-16 16:20:36 -0500

    Add matrix tests

--- rasgl/src/core/include/rasgl/core/matrix.h
+++ rasgl/src/core/include/rasgl/core/matrix.h
@@ -3,7 +3,9 @@
 
 #include "fixed_maths.h"
 #include <stdint.h>
+#include <string.h>
 
+void mat_set_identity_4x4(int32_t s1[4][4]);
 void mat_mul_3x3_3x1(int32_t s1[3][3], int32_t* s2, int32_t* dest);
 void mat_mul_4x4_4x1(int32_t s1[4][4], int32_t* s2, int32_t* dest);
 
