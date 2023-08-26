d4a998cebc0746350bf7609be66c60f5cf44b9e3
William Jones <wtravisjones@gmail.com>
2023-08-26 17:20:42 -0500

    Rotate world to viewer

--- rasgl/src/demo/demo_main.c
+++ rasgl/src/demo/demo_main.c
@@ -11,7 +11,7 @@
 #define MAX_WORLD_POINTS 1024
 #define CELL_UNITS INT_32_TO_FIXED_16_16(10)
 #define ZOOM_SPEED float_to_fixed_16_16(.05)
-#define ROTATION_SPEED 4
+#define ROTATION_SPEED 1
 #define VIEWER_SPEED float_to_fixed_16_16(.5)
 #define PROJECTION_RATIO -float_to_fixed_16_16(2.0)
 
@@ -108,13 +108,28 @@ void origin_to_world(WorldState* world_state)
 void xform_to_view(WorldState* world_state, RenderState* render_state, Point3f* viewer_pos)
 {
     int32_t view_matrix[4][4];
+    int32_t rot_matrix[4][4];
     int32_t view_point[4];
     char buffer[255];
+
     mat_set_identity_4x4(view_matrix);
+    mat_set_identity_4x4(rot_matrix);
 
     view_matrix[0][3] = -viewer_pos->x;
     view_matrix[1][3] = -viewer_pos->y;
     view_matrix[2][3] = -viewer_pos->z;
+
+    int32_t angle = (viewer_angle + 180) % 360;
+    if (angle < 0) {
+        angle += 360;
+    }
+    int32_t c = cos_table[angle];
+    int32_t s = sin_table[angle];
+
+    rot_matrix[0][0] = c;
+    rot_matrix[0][2] = s;
+    rot_matrix[2][0] = -s;
+    rot_matrix[2][2] = c;
 
     size_t* num_points = &render_state->num_points;
     size_t* num_commands = &render_state->num_commands;
@@ -122,17 +137,28 @@ void xform_to_view(WorldState* world_state, RenderState* render_state, Point3f* 
     for (int i = 0; i < world_state->num_points; i++) {
         Point3f* world_point = &world_state->points[i];
 
-        int32_t v[4] = {
+        int32_t world_vec[4] = {
             world_point->x,
             world_point->y,
             world_point->z,
             INT_32_TO_FIXED_16_16(1)
         };
 
+        // Translate world to viewer
         ras_log_trace("view trans matrix: %s\n", repr_mat_4x4(buffer, sizeof buffer, view_matrix));
-        ras_log_trace("view trans before: %s\n", repr_mat_4x1(buffer, sizeof buffer, v));
-        mat_mul_4x4_4x1(view_matrix, v, view_point);
-        ras_log_trace("view trans: %s\n", repr_mat_4x1(buffer, sizeof buffer, view_point));
+        ras_log_trace("view trans before: %s\n", repr_mat_4x1(buffer, sizeof buffer, world_vec));
+
+        int32_t translated_vec[4];
+        mat_mul_4x4_4x1(view_matrix, world_vec, translated_vec);
+        ras_log_trace("view trans after: %s\n", repr_mat_4x1(buffer, sizeof buffer, translated_vec));
+
+        // Transform world to viewer
+        ras_log_trace("rot trans matrix: %s\n", repr_mat_4x4(buffer, sizeof buffer, rot_matrix));
+        ras_log_trace("rot trans before: %s\n", repr_mat_4x1(buffer, sizeof buffer, translated_vec));
+
+        mat_mul_4x4_4x1(rot_matrix, translated_vec, view_point);
+
+        ras_log_trace("rot trans after: %s\n", repr_mat_4x1(buffer, sizeof buffer, view_point));
 
         Point3f transformed = {
             .x = view_point[0],
