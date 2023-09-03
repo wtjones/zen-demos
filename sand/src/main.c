773c563be6055e7c222b5fbb9773ac48f3eab712
William Jones <wtravisjones@gmail.com>
2023-09-02 20:58:29 -0500

    Initial sand board loop

--- sand/src/main.c
+++ sand/src/main.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <time.h>
@@ -8,18 +9,66 @@
 #define SCREEN_HEIGHT 480
 #define LOGICAL_WIDTH SCREEN_WIDTH / 4
 #define LOGICAL_HEIGHT SCREEN_HEIGHT / 4
+#define BOARD_WIDTH LOGICAL_WIDTH
+#define BOARD_HEIGHT LOGICAL_HEIGHT
+#define SIM_RATE 30
+
+typedef enum CellType {
+    EMPTY = 0,
+    SAND = 1,
+    WATER = 2,
+    OOB = 3
+} CellType;
 
 int8_t board[LOGICAL_WIDTH * LOGICAL_HEIGHT];
+int32_t iteration = 0;
 
+bool is_oob(int32_t row, int32_t col)
+{
+    return (row < 0 || row >= BOARD_HEIGHT || col < 0 || col >= BOARD_WIDTH);
+}
+int8_t get_cell(int32_t row, int32_t col)
+{
+    if (is_oob(row, col)) {
+        return OOB;
+    }
+    return board[BOARD_WIDTH * row + col];
+}
+
+void set_cell(int32_t row, int32_t col, int8_t cell)
+{
+    assert(!is_oob(row, col));
+    board[BOARD_WIDTH * row + col] = cell;
+}
+
+void update()
+{
+    for (int row = BOARD_HEIGHT - 1; row >= 0; row--) {
+        for (int col = BOARD_WIDTH - 1; col >= 0; col--) {
+            int8_t cell = get_cell(row, col);
+
+            if (cell == SAND) {
+                if (get_cell(row + 1, col) == EMPTY) {
+                    set_cell(row, col, EMPTY);
+                    set_cell(row + 1, col, SAND);
+                }
+            }
+        }
+    }
+}
 void draw_board(SDL_Renderer* renderer)
 {
-    for (int y = 0; y < LOGICAL_HEIGHT; y++) {
-        for (int x = 0; x < LOGICAL_WIDTH; x++) {
-            int8_t c = board[LOGICAL_WIDTH * y + x];
+    for (int row = 0; row < LOGICAL_HEIGHT; row++) {
+        for (int col = 0; col < LOGICAL_WIDTH; col++) {
+            int8_t cell = get_cell(row, col);
 
-            SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-            if (c > 0) {
-                SDL_RenderDrawPoint(renderer, x, y);
+            if (cell != EMPTY) {
+                if (cell == SAND) {
+                    SDL_SetRenderDrawColor(renderer, 0, 150, 150, 255);
+                } else {
+                    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+                }
+                SDL_RenderDrawPoint(renderer, col, row);
             }
         }
     }
@@ -43,15 +92,9 @@ int main(int argc, char* argv[])
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    for (int i = 0; i < LOGICAL_WIDTH; ++i)
-        SDL_RenderDrawPoint(renderer, i, i);
 
-    board[50 * LOGICAL_WIDTH + 10] = 1;
-    board[20 * LOGICAL_WIDTH + 10] = 1;
-    draw_board(renderer);
-
-    SDL_RenderPresent(renderer);
+    printf("board rows: %d, cols: %d\n", BOARD_HEIGHT, BOARD_WIDTH);
+    set_cell(0, BOARD_WIDTH / 2, SAND);
 
     int last_frame = SDL_GetTicks();
     while (!should_quit) {
@@ -67,8 +110,17 @@ int main(int argc, char* argv[])
         }
         int num_keys;
         const Uint8* keys = SDL_GetKeyboardState(&num_keys);
+        printf("iteration: %d\n", iteration);
 
-        while (SDL_GetTicks() - last_frame < 1000 / 60) {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        update();
+        draw_board(renderer);
+
+        iteration++;
+
+        SDL_RenderPresent(renderer);
+        while (SDL_GetTicks() - last_frame < 1000 / SIM_RATE) {
         }
         last_frame = SDL_GetTicks();
     }
