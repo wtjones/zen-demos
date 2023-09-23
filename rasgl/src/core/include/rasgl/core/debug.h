f295448a23ed5d9ff8c46e19c9877f4587caf35b
William T Jones <wtravisjones@gmail.com>
2023-09-23 14:03:19 -0500

    Allow for Linux debugging

--- rasgl/src/core/include/rasgl/core/debug.h
+++ rasgl/src/core/include/rasgl/core/debug.h
@@ -4,21 +4,27 @@
 #include "log.c/src/log.h"
 #include "repr.h"
 
-#ifndef DEBUG
-#    define DEBUG 0
+#ifndef DEBUG1
+#    define DEBUG1 0
 #endif
 
-#if (DEBUG)
-#    define RAS_LOG_LEVEL LOG_TRACE
-#else
-#    define RAS_LOG_LEVEL LOG_INFO
+#if defined(DEBUG1)
+#    if (DEBUG1 == 1)
+#        define RAS_LOG_LEVEL LOG_TRACE
+#    else
+#        define RAS_LOG_LEVEL LOG_INFO
+#    endif
 #endif
 
 #define debug_print(fmt, ...)                  \
     do {                                       \
-        if (DEBUG)                             \
+        if (DEBUG1)                            \
             fprintf(stderr, fmt, __VA_ARGS__); \
     } while (0)
+
+#ifndef __FILE_NAME__
+#    define __FILE_NAME__ ""
+#endif
 
 #define ras_log_trace(...) log_log(LOG_TRACE, __FILE_NAME__, __LINE__, __VA_ARGS__)
 #define ras_log_debug(...) log_log(LOG_DEBUG, __FILE_NAME__, __LINE__, __VA_ARGS__)
