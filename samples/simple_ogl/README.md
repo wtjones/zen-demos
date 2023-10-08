47b7f9c4b15cd90a7de596e8997a6f32adb17b17
William T Jones <wtravisjones@gmail.com>
2023-10-08 14:05:10 -0500

    Add simple OpenGL example

---
+++ samples/simple_ogl/README.md
@@ -0,0 +1,6 @@
+Simple SDL2 + legacy OpenGL example
+
+## References
+
+https://marcelfischer.eu/blog/2019/sdl-opengl-cmake/
+https://lazyfoo.net/tutorials/SDL/50_SDL_and_opengl_2/index.php
---
+++ samples/simple_ogl/src/main.c
@@ -0,0 +1,104 @@
+#include <GL/glu.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <stdbool.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+SDL_GLContext gl_context;
+
+bool init_gl()
+{
+    GLenum error = GL_NO_ERROR;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    error = glGetError();
+    if (error != GL_NO_ERROR) {
+        printf("Error initializing OpenGL! %s\n", gluErrorString(error));
+        return false;
+    }
+    return true;
+}
+
+void render()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_QUADS);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f(0.5f, -0.5f);
+    glVertex2f(0.5f, 0.5f);
+    glVertex2f(-0.5f, 0.5f);
+    glEnd();
+}
+
+int main(int argc, char* argv[])
+{
+
+    SDL_bool should_quit = SDL_FALSE;
+    char str[80];
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("SDL error \n");
+        return 1;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    SDL_Window* win = SDL_CreateWindow("Simple Demo", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    gl_context = SDL_GL_CreateContext(win);
+
+    if (gl_context == NULL) {
+        printf("OpenGL context could not be created. SDL Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    if (SDL_GL_SetSwapInterval(1) < 0) {
+        printf("Unable to set swap interval. SDL Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    if (!init_gl()) {
+        printf("OpenGL init error\n");
+        return 1;
+    }
+
+    int last_frame = SDL_GetTicks();
+    while (!should_quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                should_quit = SDL_TRUE;
+                break;
+            case SDL_KEYUP:
+                should_quit = event.key.keysym.scancode == SDL_SCANCODE_ESCAPE;
+            }
+        }
+        int num_keys;
+        const Uint8* keys = SDL_GetKeyboardState(&num_keys);
+
+        while (SDL_GetTicks() - last_frame < 1000 / 60) {
+        }
+        last_frame = SDL_GetTicks();
+
+        render();
+        SDL_GL_SwapWindow(win);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+
+    SDL_Quit();
+    return 0;
+}
