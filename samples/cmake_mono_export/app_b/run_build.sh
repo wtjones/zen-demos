f974b3a648b398fd42535508be3f6b11602056a7
William T Jones <wtravisjones@gmail.com>
2024-05-06 21:01:22 -0500

    cmake-mono: Use find_library() hint
    Avoid hard-coding the library .so path

--- samples/cmake_mono_export/app_b/run_build.sh
+++ samples/cmake_mono_export/app_b/run_build.sh
@@ -4,6 +4,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 rm -rf ${SCRIPT_DIR}/build
 
-cmake -S . -B build -DCMAKE_PREFIX_PATH=../lib_a/build
-#cmake -S . -B build
+cmake -S . -B build
 cmake --build build
