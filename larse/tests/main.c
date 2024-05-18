22d4a4d056e1cac8be36c56e8593184f9495349d
William T Jones <wtravisjones@gmail.com>
2024-05-17 20:17:19 -0500

    larse: POC of using Unity test
    Test discovery isn't working. Something like this would
    be needed, but was getting errors with latest unity:
    https://github.com/omsharp/CMade/blob/main/_cmake/Tests.cmake

--- larse/tests/main.c
+++ larse/tests/main.c
@@ -1,91 +1,112 @@
 #include "larse/core/expression.h"
 #include "larse/core/parse.h"
 #include "larse/core/repr.h"
-#include "test_support.h"
+// #include "test_support.h"
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <unity.h>
 
-int test_parse_single()
+void test_AverageThreeBytes_should_AverageHighValues(void)
 {
-
-    LarNode* node;
-
-    LarParseResult result;
-    result = lar_parse_single("(\"test\")", &node);
-    assert(strcmp(node->list.nodes[0].atom.val_string, "test") == 0);
-    printf("got string: %s\n", node->list.nodes[0].atom.val_string);
-    return 0;
+    TEST_ASSERT_EQUAL_HEX8(80, 80);
 }
 
-int test_parse_symbol()
+void test_anotherthing(void)
 {
-
-    LarNode* node;
-
-    LarParseResult result;
-    result = lar_parse_single("(my-symbol 1 3)", &node);
-    assert(strcmp(node->list.nodes[0].atom.val_symbol, "my-symbol") == 0);
-    return 0;
+    TEST_ASSERT_EQUAL_HEX8(80, 80);
 }
 
-int test_repr()
+int main(void)
 {
-    const char* exp1_in = "(this :here\n"
-                          "(is math)\n"
-                          "(+ 1 2))";
+    UNITY_BEGIN();
 
-    const char* expected = "[List]\n"
-                           "  this [Atom: symbol]\n"
-                           "  :here [Atom: symbol]\n"
-                           "  [List]\n"
-                           "    is [Atom: symbol]\n"
-                           "    math [Atom: symbol]\n"
-                           "  [List]\n"
-                           "    + [Atom: symbol]\n"
-                           "    1 [Atom: integer]\n"
-                           "    2 [Atom: integer]";
+    RUN_TEST(test_AverageThreeBytes_should_AverageHighValues);
+    RUN_TEST(test_anotherthing);
 
-    LarNode* node1;
-    LarParseResult result;
-    result = lar_parse_single(exp1_in, &node1);
-    assert(result == LAR_PARSE_RESULT_OK);
-    char* actual = lar_repr_expression(node1);
-    printf("repr test:\n\nexpected:\n%s\n\nactual:\n%s\n", expected, actual);
-    bool pass = (strcmp(expected, actual) == 0);
-    free(actual);
-    return !pass;
+    return UNITY_END();
 }
 
-int test_parse()
-{
-    return 0;
-}
+// int test_parse_single()
+// {
 
-TestFn test_fns[] = {
-    { "TEST_PARSE_SYMBOL", test_parse_symbol },
-    { "TEST_PARSE_SINGLE", test_parse_single },
-    { "TEST_PARSE", test_parse },
-    { "TEST_REPR", test_repr }
-};
+//     LarNode* node;
 
-int main(int argc, const char** argv)
-{
+//     LarParseResult result;
+//     result = lar_parse_single("(\"test\")", &node);
+//     assert(strcmp(node->list.nodes[0].atom.val_string, "test") == 0);
+//     printf("got string: %s\n", node->list.nodes[0].atom.val_string);
+//     return 0;
+// }
 
-    if (argc == 1) {
-        fprintf(stderr, "Test name required.\n");
-        return 1;
-    }
+// int test_parse_symbol()
+// {
 
-    for (int i = 1; i < argc; ++i) {
-        printf("main(): argv[%d]: %s\n", i, argv[i]);
-    }
-    size_t num_fns = sizeof(test_fns) / sizeof(test_fns[0]);
+//     LarNode* node;
 
-    TestFn* test_fn = test_fn_lookup(test_fns, num_fns, argv[1]);
-    if (test_fn == NULL) {
-        fprintf(stderr, "Error: Test %s not found.\n", argv[1]);
-        return 1;
-    }
-    return test_fn->fn();
-}
+//     LarParseResult result;
+//     result = lar_parse_single("(my-symbol 1 3)", &node);
+//     assert(strcmp(node->list.nodes[0].atom.val_symbol, "my-symbol") == 0);
+//     return 0;
+// }
+
+// int test_repr()
+// {
+//     const char* exp1_in = "(this :here\n"
+//                           "(is math)\n"
+//                           "(+ 1 2))";
+
+//     const char* expected = "[List]\n"
+//                            "  this [Atom: symbol]\n"
+//                            "  :here [Atom: symbol]\n"
+//                            "  [List]\n"
+//                            "    is [Atom: symbol]\n"
+//                            "    math [Atom: symbol]\n"
+//                            "  [List]\n"
+//                            "    + [Atom: symbol]\n"
+//                            "    1 [Atom: integer]\n"
+//                            "    2 [Atom: integer]";
+
+//     LarNode* node1;
+//     LarParseResult result;
+//     result = lar_parse_single(exp1_in, &node1);
+//     assert(result == LAR_PARSE_RESULT_OK);
+//     char* actual = lar_repr_expression(node1);
+//     printf("repr test:\n\nexpected:\n%s\n\nactual:\n%s\n", expected, actual);
+//     bool pass = (strcmp(expected, actual) == 0);
+//     free(actual);
+//     return !pass;
+// }
+
+// int test_parse()
+// {
+//     return 0;
+// }
+
+// TestFn test_fns[] = {
+//     { "TEST_PARSE_SYMBOL", test_parse_symbol },
+//     { "TEST_PARSE_SINGLE", test_parse_single },
+//     { "TEST_PARSE", test_parse },
+//     { "TEST_REPR", test_repr }
+// };
+
+// int main(int argc, const char** argv)
+// {
+
+//     if (argc == 1) {
+//         fprintf(stderr, "Test name required.\n");
+//         return 1;
+//     }
+
+//     for (int i = 1; i < argc; ++i) {
+//         printf("main(): argv[%d]: %s\n", i, argv[i]);
+//     }
+//     size_t num_fns = sizeof(test_fns) / sizeof(test_fns[0]);
+
+//     TestFn* test_fn = test_fn_lookup(test_fns, num_fns, argv[1]);
+//     if (test_fn == NULL) {
+//         fprintf(stderr, "Error: Test %s not found.\n", argv[1]);
+//         return 1;
+//     }
+//     return test_fn->fn();
+// }
