e560926fc12231159400612fd3969904f669a981
William T Jones <wtravisjones@gmail.com>
2024-11-09 16:52:56 -0600

    ras: Add tmux setup

---
+++ rasgl/run_tmux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+tmux new-session -d -s ras_session
+tmux split-window -v
+tmux select-pane -t 1
+tmux send-keys "tail -f /tmp/rasgl.log" C-m
+tmux select-pane -t 0
+tmux attach-session -t ras_session
