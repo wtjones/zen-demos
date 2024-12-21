291c0efc9c3fea8ec007b03d3048e1d6249f8789
William T Jones <wtravisjones@gmail.com>
2024-12-21 15:51:14 -0600

    ras: Encode buffered log messages to split calls
    Allows buffered messages to route to individual log_log() calls
    rather than a single log_info().

--- rasgl/src/core/debug.c
+++ rasgl/src/core/debug.c
@@ -1,32 +1,58 @@
 
 #include "rasgl/core/debug.h"
+#include <assert.h>
 #include <string.h>
 
-char log_buffer[RAX_MAX_LOG_BUFFER];
+char log_buffer[RAS_MAX_LOG_BUFFER];
+size_t log_buffer_offset = 0;
 
 void ras_log_flush()
 {
     ras_log_trace("flushing buffer");
-    ras_log_info(log_buffer);
+
+    // Decode The buffer format of {level}{message}{\0}
+    size_t offset = 0;
+    while (offset < log_buffer_offset) {
+        int level;
+        memcpy(&level, log_buffer + offset, sizeof(level));
+        offset += sizeof(level);
+
+        const char* message = log_buffer + offset;
+        size_t message_length = strlen(message) + 1;
+        offset += message_length;
+
+        // File and line number not available
+        log_log(level, "", 0, message);
+    }
 }
 
 void ras_log_clear()
 {
     log_buffer[0] = '\0';
+    log_buffer_offset = 0;
 }
 
 void core_log_buffer(int level, const char* file, int line, const char* fmt, ...)
 {
-    if (level < log_get_level()) {
-        return;
-    }
-
     char buffer[1000];
     va_list args;
 
     va_start(args, fmt);
     vsnprintf(buffer, sizeof buffer, fmt, args);
     va_end(args);
-    strcat(buffer, "\n");
-    strcat(log_buffer, buffer);
+
+    // Validate based on size of {level}{message}{\0}
+    if (log_buffer_offset + sizeof(level) + strlen(buffer) + 1 > RAS_MAX_LOG_BUFFER) {
+        log_error("Log buffer overflow! %d\n", strlen(log_buffer));
+
+        return;
+    }
+
+    // Prefix the log level to the buffer
+    memcpy(log_buffer + log_buffer_offset, &level, sizeof(level));
+    log_buffer_offset += sizeof(level);
+
+    size_t message_length = strlen(buffer) + 1; // +1 for the null terminator
+    memcpy(log_buffer + log_buffer_offset, buffer, message_length);
+    log_buffer_offset += message_length;
 }
--- rasgl/src/core/include/rasgl/core/debug.h
+++ rasgl/src/core/include/rasgl/core/debug.h
@@ -3,7 +3,7 @@
 
 #include "log.c/src/log.h"
 
-#define RAX_MAX_LOG_BUFFER 100000
+#define RAS_MAX_LOG_BUFFER 100000
 
 // FIXME: needed for vscode intellisense
 #ifndef __FILE_NAME__
