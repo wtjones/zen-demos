8928fa9e0d46c9f2db6e99e4e6a63ed5b56ec480
William T Jones <wtravisjones@gmail.com>
2025-01-01 21:07:29 -0600

    cl: Refactor catalog example to support id references

--- samples/cl/catalog.lisp
+++ samples/cl/catalog.lisp
@@ -1,61 +1,78 @@
-(defvar library-shelf `(elder-race ancient-world))
-(defvar my-shelf `())
+(defstruct catalog-book
+  (id 0 :type integer)
+  title
+  isbn)
+(defstruct book-checkout
+  patron-id
+  book-id
+  (occurred-at (get-universal-time) :type integer))
 
-(defun print-stock ()
-  (format t "Library shelf: ~a~%" library-shelf)
-  (format t "My shelf: ~a~%" my-shelf))
+(defstruct patron
+  (id 0 :type integer)
+  name
+  (enrolled-at (get-universal-time) :type integer))
 
-(defun checkout-book (name)
-  (if (member name library-shelf)
+(defconstant max-checkouts 3)
+(defvar *catalog-books* nil)
+(defvar *catalog-book-checkouts* nil)
+(defvar *patrons* nil)
+(defvar *last-id* 0)
+
+(defun get-id ()
+  (setf *last-id* (+ 1 *last-id*))
+  *last-id*)
+
+(defun print-catalog ()
+  (format t "Catalog: ~a~%" *catalog-books*))
+(defun print-catalog-checkouts ()
+  (format t "Checked out: ~a~%" *catalog-book-checkouts*))
+
+; TODO: prevent dupe checkouts to patron
+(defun checkout-book (book-id patron-id)
+  (if (not (find book-id *catalog-books* :key #'catalog-book-id))
       (progn
-       (setf library-shelf (remove name library-shelf :count 1))
-       (setf my-shelf (cons name my-shelf))
-       (format t "Checked out ~a~%" name))
-      (format t "Sorry, ~a is not available~%" name)))
+       (format t "Book ~a not found~%" book-id)
+       (return-from checkout-book)))
+  (if (find book-id *catalog-book-checkouts* :key #'book-checkout-book-id)
+      (progn
+       (format t "Book ~a is already checked out~%" book-id)
+       (return-from checkout-book)))
+  (format t "Checking out ~a to ~a~%" book-id patron-id)
+  (setf *catalog-book-checkouts*
+    (cons
+      (make-book-checkout :patron-id patron-id :book-id book-id)
+      *catalog-book-checkouts*)))
+
+; TODO: validate id
+(defun intake-books (books)
+  (setf *catalog-books* (append books *catalog-books*))
+  (length books))
 
 
-(defun stock-book (name)
-  (if (listp name)
-      (progn
-       (setf library-shelf (append name library-shelf))
-       (format t "Books ~a are now stocked~%" name)
-       (length name))
-      (progn
-       (setf library-shelf (cons name library-shelf))
-       (format t "Book ~a is now stocked~%" name)
-       1)))
+; TODO: validate id
+(defun enroll-patrons (patrons)
+  (setf *patrons* (append patrons *patrons*)))
 
-(defun return-book (name)
-  (if (member name my-shelf)
-      (progn
-       (setf my-shelf (remove name my-shelf))
-       (setf library-shelf (cons name library-shelf))
-       (format t "Returned ~a~%" name))
-      (format t "Sorry, ~a is not in your shelf~%" name)))
 
 (defun test ()
-  (setf library-shelf '())
-  (setf my-shelf `())
+  (setf *catalog-books* '())
+  (setf *catalog-book-checkouts* '())
 
-  ; add a list
-  (let ((count-1 (stock-book '(elder-race ancient-world))))
+  (enroll-patrons (list
+                   (make-patron :id (get-id) :name "Abe")
+                   (make-patron :id (get-id) :name "Brine")))
+
+  (let ((count-1 (intake-books (list (make-catalog-book :id (get-id) :title 'the-hobbit :isbn "0-306-40615-2")
+                                     (make-catalog-book :id (get-id) :title 'dune :isbn "0-406-40615-2")
+                                     (make-catalog-book :id (get-id) :title 'dune :isbn "0-406-40615-2")
+                                     (make-catalog-book :id (get-id) :title 'elder-race :isbn "0-506-40615-2")
+                                     (make-catalog-book :id (get-id) :title 'sapiens :isbn "0-606-40615-2")))))
     (format t "Returned: ~a~%" count-1))
 
-  ; add an atom
-  (let ((count-1 (stock-book 'the-hobbit)))
-    (format t "Returned: ~a~%" count-1))
+  (let ((book-result (first *catalog-books*)))
+    (format t "Found book: ~a~%" book-result)
+    (checkout-book (catalog-book-id book-result) 44)
+    (checkout-book (catalog-book-id book-result) 44))
 
-  ; add a dupe
-  (let ((count-1 (stock-book 'the-hobbit)))
-    (format t "Returned: ~a~%" count-1))
-
-  (print-stock)
-
-  (checkout-book `money-ball)
-  (checkout-book `the-hobbit)
-  (checkout-book `ancient-world)
-  (print-stock)
-
-  (return-book `the-hobbit)
-
-  (print-stock))
+  (print-catalog)
+  (print-catalog-checkouts))
