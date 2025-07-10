fe5be87d9cbbf1a0738032c5623e6514bfe1ea62
William T Jones <wtravisjones@gmail.com>
2025-07-09 20:37:31 -0500

    ras: Enable sanitizer in SDL build

--- rasgl/run_posix.sh
+++ rasgl/run_posix.sh
@@ -5,6 +5,11 @@ DEBUG=${2:-0}
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 rm -rf ${SCRIPT_DIR}/build
-cmake -S . -DRAS_PLATFORM=ras_sdl -DRAS_DEMO=$DEMO -DDEBUG=$DEBUG -B build
+cmake -S . \
+  -DRAS_PLATFORM=ras_sdl \
+  -DRAS_DEMO=$DEMO \
+  -DDEBUG=$DEBUG \
+  -DENABLE_ASAN=ON \
+  -B build
 cmake --build build
-./build/bin/demo $3
+ASAN_OPTIONS=detect_leaks=0 ./build/bin/demo $3
