a07897761076e1e8be005ee2f2415470a6c2203a
William T Jones <wtravisjones@gmail.com>
2025-12-28 20:33:25 -0600

    ras: run_posix.sh exits when build fails

--- rasgl/run_posix.sh
+++ rasgl/run_posix.sh
@@ -7,4 +7,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ${SCRIPT_DIR}/build_posix.sh $DEMO $DEBUG
 
+if [ $? -ne 0 ]; then
+    echo "Build failed"
+    exit 1
+fi
+
 ASAN_OPTIONS=detect_leaks=0 ./build/bin/demo $3
