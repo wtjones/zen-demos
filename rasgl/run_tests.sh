a32ea16a4b9c6aa670d408136c1e12d7684275a5
William T Jones <wtravisjones@gmail.com>
2026-02-15 13:45:14 -0600

    ras: Remove build dir purge in tests

--- rasgl/run_tests.sh
+++ rasgl/run_tests.sh
@@ -3,7 +3,7 @@
 DEBUG=${1:-0}
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-rm -rf ${SCRIPT_DIR}/build
+
 cmake -S . -DDEBUG=$DEBUG -DENABLE_ASAN=ON -B build
 cmake --build build
 ./build/bin/tests
