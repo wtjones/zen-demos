61d1ce84824dd689a9fdd8115dd79d5dbc8d7fe4
William T Jones <wtravisjones@gmail.com>
2026-06-14 14:24:28 -0500

    larse: Support ctest name as param

--- larse/run_tests.sh
+++ larse/run_tests.sh
@@ -2,10 +2,16 @@
 
 set -euo pipefail
 
+if [[ -z ${1:-} ]]; then
+	TEST_ARG=""
+else
+	TEST_ARG="-R ${1}"
+fi
+
 main() {
     cmake -S . -Bbuild -DENABLE_ASAN=ON
     cmake --build build
-    env CTEST_OUTPUT_ON_FAILURE=TRUE ctest --test-dir build --verbose
+    env CTEST_OUTPUT_ON_FAILURE=TRUE ctest --test-dir build --verbose $TEST_ARG
 }
 
 main
