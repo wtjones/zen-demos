8995e0a025570530cb0b91a26288c509a52ab07a
William T Jones <wtravisjones@gmail.com>
2026-07-14 21:09:51 -0500

    ras: Don't nest tmux

--- rasgl/run_tmux.sh
+++ rasgl/run_tmux.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-tmux new-session -d -s ras_session
-tmux split-window -v
-tmux select-pane -t 1
-tmux send-keys "touch /tmp/rasgl.log && tail -f /tmp/rasgl.log" C-m
-tmux select-pane -t 0
-tmux attach-session -t ras_session
+SESSION="ras_session"
+
+if ! tmux has-session -t "$SESSION" 2>/dev/null; then
+    tmux new-session -d -s "$SESSION"
+fi
+
+tmux split-window -v -t "$SESSION"
+tmux send-keys -t "$SESSION":0.1 'touch /tmp/rasgl.log && tail -f /tmp/rasgl.log' C-m
+tmux select-pane -t "$SESSION":0.0
+
+if [ -n "$TMUX" ]; then
+    exec tmux switch-client -t "$SESSION"
+else
+    exec tmux attach-session -t "$SESSION"
+fi
